383c17ad15521c3c5a9bf4763554d46ccea17f9a
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-10-30 13:06:08 +0530

    Create Product model and its validations
    This commit creates the Product model with three parameters,
    name, description and price. It also adds validation
    for presence of name and price.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027164024) do
+ActiveRecord::Schema.define(version: 20171030072650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,13 @@ ActiveRecord::Schema.define(version: 20171027164024) do
     t.integer "depth"
     t.integer "children_count"
   end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
