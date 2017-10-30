0c5683451341de8a312c74518bc233610283df07
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-10-30 18:00:26 +0530

    Add validation for uniqueness of each product in a category

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,4 +1,5 @@
 class Categorization < ApplicationRecord
   belongs_to :category
   belongs_to :product
+  validates_uniqueness_of :product_id, scope: :category_id
 end
--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Categorization, type: :model do
   it { should belong_to :category }
   it { should belong_to :product }
+  describe 'validation' do
+    it 'validates the uniqueness of each product in a category' do
+      subject.product = FactoryBot.create :product
+      is_expected.to validate_uniqueness_of(:product_id).scoped_to(:category_id)
+    end
+  end
 end
