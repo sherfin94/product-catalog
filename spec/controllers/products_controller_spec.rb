5bec9d9c5fedacc5cdea5537d1762f33162c2e98
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-02 20:24:15 +0530

    Display all product names as response to ProductsController#index
    This commit makes ProductsController#index action pass the details
    of all products to the corresponding view 'index'. It also makes
    that index view display all the names it received.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'assigns @products with details of products' do
+      products = Array.new(5) { FactoryBot.create :product }
+      get :index
+      expect(assigns(:products)).to eq products
+    end
+  end
 end
--- spec/views/products/index.html.slim_spec.rb
+++ spec/views/products/index.html.slim_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "products/index.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'products/index.html.slim', type: :view do
+  def stubbed_products(product_names)
+    product_names.map do |product_name|
+      FactoryBot.build_stubbed :product, name: product_name
+    end
+  end
+
+  it 'displays all the product names' do
+    product_names = %w[something jacket pen tie bottle]
+    assign :products, stubbed_products(product_names)
+
+    render
+
+    expect(rendered).to include(product_names[rand(5)])
+  end
 end
