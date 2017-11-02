17a897152f195aa2c749c3edfc8f292c77688d3b
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-02 22:32:51 +0530

    Display product description and price in ProductsController#index
    This commit makes the view rendered by ProductsController#index
    display the description and price of each product below its name.

--- spec/views/products/index.html.slim_spec.rb
+++ spec/views/products/index.html.slim_spec.rb
@@ -1,18 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'products/index.html.slim', type: :view do
-  def stubbed_products(product_names)
-    product_names.map do |product_name|
-      FactoryBot.build_stubbed :product, name: product_name
+  def stubbed_products(names, descriptions, prices)
+    names.zip(descriptions, prices).map do |name, description, price|
+      FactoryBot.build_stubbed(
+        :product,
+        name: name,
+        description: description,
+        price: price
+      )
     end
   end
 
-  it 'displays all the product names' do
-    product_names = %w[something jacket pen tie bottle]
-    assign :products, stubbed_products(product_names)
+  it 'displays all the product names, descriptions and prices' do
+    names = %w[something jacket pen tie bottle]
+    descriptions = %w[lorem ipsum dolor sit amet]
+    prices = %w[2.45 6.43 0.3 1.9 10.99]
+    assign :products, stubbed_products(names, descriptions, prices)
 
     render
 
-    expect(rendered).to include(product_names[rand(5)])
+    names.zip(descriptions, prices).each do |name, description, price|
+      expect(rendered).to include(name)
+      expect(rendered).to include(description)
+      expect(rendered).to include(price)
+    end
   end
 end
