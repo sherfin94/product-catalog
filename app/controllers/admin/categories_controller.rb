ae7dcae3b978f1e636663c544fe4d513ca14384a
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-12 00:36:43 +0530

    Pass hierarchically ordered category list to Admin::Categories#index view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @hierarchically_ordered_categoryies =
+      HierarchicallyOrderedCategoriesQuery.new.all
   end
 end
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Admin::CategoriesController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it 'assigns @hierarchically_ordered_categoryies with the result of
+      HierarchicallyOrderedCategoriesQuery#all' do
+        place_holder_categories = Array.new(2) { FactoryBot.build :category }
+        allow_any_instance_of(HierarchicallyOrderedCategoriesQuery)
+          .to receive(:all)
+          .and_return place_holder_categories
+
+        get :index
+        expect(assigns(:hierarchically_ordered_categoryies))
+          .to eq place_holder_categories
+      end
+  end
+
 end
