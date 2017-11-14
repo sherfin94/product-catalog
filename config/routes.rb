652a6fd114388194cff08c0445a51ae930e9a2b7
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-14 11:39:08 +0530

    Use resources helper to route Products actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   get 'admin/index'
 
   namespace :products do
-    get 'filtered/index/:categories', to: 'filtered#index'
+    resources :filtered, only: %i[index]
   end
 
-  get 'products/index'
+  resources :products, only: %i[index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
