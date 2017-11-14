c6e96b0c457625ab614916e9464e1903409baf85
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-15 01:56:40 +0530

    Display all product information on Admin::Products#index

--- spec/views/admin/products/index.html.slim_spec.rb
+++ spec/views/admin/products/index.html.slim_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "products/index.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'displays all the products names, descriptions and prices' do
+    products = Array.new(5) { FactoryBot.build :product }
+    assign :products, products
+
+    render template: 'admin/products/index.html.slim'
+
+    expect(rendered).to include(*(products.pluck :name))
+    expect(rendered).to include(*(products.pluck :description))
+    expect(rendered).to include(*(products.pluck :price).map(&:to_s))
+  end
 end
