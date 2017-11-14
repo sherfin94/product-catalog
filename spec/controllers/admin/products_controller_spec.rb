6615cf0235a061cd6ee30147f5b80c938270cfa3
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-15 01:28:34 +0530

    Pass an instance of Product to Admin::Products#new view

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -20,6 +20,11 @@ RSpec.describe Admin::ProductsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it 'passes an instance of Product through @product' do
+      get :new
+      expect(assigns(:product)).to be_instance_of Product
+    end
   end
 
   describe "POST #create" do
