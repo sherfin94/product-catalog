33975197801f18748297532c344cf56902dee7dc
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-11-14 21:56:04 +0530

    Create Admin::ProductsController
    This commit creates a new controller named ProductsController
    namespaced to Admin. It also adds index, new, create and destroy
    actions, alongwith its corresponding views and assets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :categories, only: %i[index new create destroy]
+    resources :products, only: %i[index new create destroy]
   end
-
+  
   resources :admin, only: %i[index]
 
   namespace :products do
@@ -10,6 +11,4 @@ Rails.application.routes.draw do
   end
 
   resources :products, only: %i[index]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
